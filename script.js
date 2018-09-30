bf3ca0ff3668754334b4341d430ffc1a9a859f95
VolkenK <volkenkoelewijn@gmail.com>
2018-09-30 22:23:34 +0200

    Add modals

--- script.js
+++ script.js
@@ -1,43 +1,38 @@
-// const canvas = document.getElementById('canvas');
-// const context = canvas.getContext("2d");
-// let body = document.querySelector('body');
-// let leftPadding = 0;
-// setInterval(() => {
-//     leftPadding++;
-//     body.style.backgroundPositionX = leftPadding + 'px';
-//     if (leftPadding > 10000) {
-//         leftPadding = 0;
-//     }
-// }, 25);
-
-
-let profileScroller = document.querySelector('#nav_profile');
-let projectScroller = document.querySelector('#nav_project');
-let contactScroller = document.querySelector('#nav_contact');
 let profile = document.querySelector('#profile');
 let project = document.querySelector('#projects');
 let contact = document.querySelector('#contact');
 
-profileScroller.addEventListener("click", scrollToProfile);
-projectScroller.addEventListener("click", scrollToProjects);
-contactScroller.addEventListener("click", scrollToContact);
-
-function scrollToProfile() {
-    smoothScroll(profile);
-}
-
-function scrollToProjects() {
-    smoothScroll(project);
-}
-
-function scrollToContact() {
-    smoothScroll(contact);
-}
+document.querySelector('#nav_profile').addEventListener("click", () => smoothScroll(profile));
+document.querySelector('#nav_project').addEventListener("click", () => smoothScroll(project));
+document.querySelector('#nav_contact').addEventListener("click", () => smoothScroll(contact));
 
 function smoothScroll(ev) {
     console.log('ながさき');
     scrollTo({
         top: ev.offsetTop - 50,
         behavior: "smooth"
+    });
+}
+
+addModal('dungeon');
+addModal('isaac');
+
+function addModal(identifier) {
+    let object = document.querySelector(`#${identifier}`);
+
+    object.querySelector('.card').addEventListener("click", () => {
+        object.querySelector(`#${identifier}_modal`).classList.add('is-active');
+    });
+
+    object.querySelector('.modal').addEventListener("click", () => {
+        object.querySelector('.modal').classList.remove('is-active');
+    });
+
+    object.querySelector('.delete').addEventListener("click", () => {
+        object.querySelector('.modal').classList.remove('is-active');
+    });
+
+    object.querySelector('.modal-card').addEventListener("click", (event) => {
+        event.stopPropagation();
     });
 }
