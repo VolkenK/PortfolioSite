bdcbf8a4ec7c47ba0b11ff7910049e03354170cd
VolkenK <volkenkoelewijn@gmail.com>
2018-11-04 18:19:47 +0100

    yeet

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 let profile = document.querySelector('#profile');
 let project = document.querySelector('#projects');
 let contact = document.querySelector('#contact');
+let games = document.querySelector('#games');
+let header = document.querySelector('.fullscreen-header');
 
 document.querySelector('#nav_profile').addEventListener("click", () => smoothScroll(profile));
 document.querySelector('#nav_project').addEventListener("click", () => smoothScroll(project));
+document.querySelector('#nav_games').addEventListener("click", () => smoothScroll(games));
 document.querySelector('#nav_contact').addEventListener("click", () => smoothScroll(contact));
 
 function smoothScroll(ev) {
     console.log('ながさき');
     scrollTo({
-        top: ev.offsetTop - 50,
+        top: ev.offsetTop + header.clientHeight - 50,
         behavior: "smooth"
     });
 }
@@ -25,8 +28,10 @@ function addModal(identifier) {
     let object = document.querySelector(`#${identifier}`);
 
     object.querySelector('.card').addEventListener("click", () => {
-        document.querySelector('html').classList.add('enable-scroll');
-        object.querySelector(`#${identifier}_modal`).classList.add('is-active');
+        window.location.href = `${identifier}.html`;
+
+        // document.querySelector('html').classList.add('enable-scroll');
+        // object.querySelector(`#${identifier}_modal`).classList.add('is-active');
     });
 
     object.querySelector('.modal').addEventListener("click", () => {
